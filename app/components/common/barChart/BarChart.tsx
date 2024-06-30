979ef48afd3349fe2c67f03fd406b3352454aafb
Basma Khalil <110143256+basma-khalil@users.noreply.github.com>
2024-06-30 08:01:53 +0300

    Update BarChart.tsx

--- app/components/common/barChart/BarChart.tsx
+++ app/components/common/barChart/BarChart.tsx
@@ -24,7 +24,7 @@ export default function BarChart({ isDashboard }: BarChartProps) {
 
   // Simulate fetching the data from an API endpoint instead of importing the data directly
   const baseUrl = process.env.NEXT_PUBLIC_TEST_URL;
-  const testUrl = 'https://admin-dashboard-8evi8vjcp-basma-khalils-projects.vercel.app/api/bar-chart';
+  const testUrl = 'https://admin-dashboard-theme.netlify.app/api/bar-chart';
   const barDataURL = `${baseUrl}/api/bar-chart`;
   const { data, isLoading, error } = useFetch<
     ApexAxisChartSeries | ApexNonAxisChartSeries | undefined
