a36a696140f7cf53d5849bb1deb17b9e172949d0
Basma Khalil <110143256+basma-khalil@users.noreply.github.com>
2024-06-30 16:52:18 +0300

    Update LineChart.tsx

--- app/components/common/lineChart/LineChart.tsx
+++ app/components/common/lineChart/LineChart.tsx
@@ -23,8 +23,7 @@ export default function LineChart({ isDashboard }: LineChartProps) {
   const { options } = useLineChartOptions(isDashboard);
 
   // Simulate fetching the data from an API endpoint instead of importing the data directly
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const lineDataURL = `${baseUrl}/api/line-chart.json`;
+  const lineDataURL = '/api/line-chart';
   const { data, isLoading, error } = useFetch<
     ApexAxisChartSeries | ApexNonAxisChartSeries | undefined
   >(lineDataURL);
